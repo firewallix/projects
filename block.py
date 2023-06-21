c77799a4c79d95dc987b37875ee71f4102c61e00
Firewall IX <70867350+firewallix@users.noreply.github.com>
2023-06-21 19:47:45 +0700

    Create block.py

---
+++ block.py
@@ -0,0 +1,8 @@
+#blok program adalah: kumpulan dari beberapa statement yang digabungkan dalam satu blok
+#penulisan blok program harus ditambahkan indentasi (tab atau spasi 2x/4x)
+nama = input("masukkan nama: ")
+print("Hello")
+if nama == "Admin":
+    print(nama)
+    print("Selamat datang")
+print("World")
